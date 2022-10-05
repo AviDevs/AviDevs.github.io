54d8b21259343e40fbe02f7ec4a1200e35d5b7a8
AviDev <avidevs543@gmail.com>
2022-10-05 15:53:00 -0400

    Bypass
    Este es un bypass para Linkerverse

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+/*!
+* Start Bootstrap - Landing Page v5.1.0 (https://startbootstrap.com/theme/landing-page)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
+*/
